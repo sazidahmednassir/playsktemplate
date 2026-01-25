4f6d49d8c7dd51eb5d2fe36eab4942bca729800e
sazidahmednassir <sazidahmednassir@gmail.com>
2026-01-25 18:34:42 +0600

    add the yml file

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
     baseURL: "https://app-test.informatiq.no/apps/owa-next/", // Base URL for the application under test
     timeout: 60000, // Set test timeout to 60 seconds
     trace: "on", // Enable tracing for debugging
-    headless: false, // Run tests in non-headless mode
+    headless: true, // Run tests in non-headless mode
     screenshot: "only-on-failure", // Capture screenshots only on test failures
     video: "retain-on-failure", // Retain video recordings only on test failures
     launchOptions: {
