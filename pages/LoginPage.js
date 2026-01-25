0fe527b192201f5d82f0f946e8f6c52ad2516b6c
sazidahmednassir <sazidahmednassir@gmail.com>
2026-01-25 21:07:04 +0600

    update the some file

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,5 +1,5 @@
 const LoginPage = {
-  getUsername: (page) => page.getByRole("textbox", { name: "Email address" }),
+  getUsername: (page) => page.locator("#username"),
   getPassword: (page) => page.getByRole("textbox", { name: "Password" }),
   getContinueBtn: (page) => page.getByRole("button", { name: "Continue" }),
   getHomePageIdentifier: (page) =>
