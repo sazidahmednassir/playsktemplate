13d59ed48105f8edad91ff17c9ddc205d7c5ac24
sazidahmednassir <sazidahmednassir@gmail.com>
2026-01-26 16:59:10 +0600

    update the playwright config and comment the yml file

--- playwright.config.js
+++ playwright.config.js
@@ -43,7 +43,7 @@ module.exports = defineConfig({
 
     timeout: 80000, // Set test timeout to 60 seconds
     trace: "on", // Enable tracing for debugging
-    headless: false, // Run tests in non-headless mode
+    headless: true, // Run tests in non-headless mode
     screenshot: "only-on-failure", // Capture screenshots only on test failures
     video: "retain-on-failure", // Retain video recordings only on test failures
     launchOptions: {
