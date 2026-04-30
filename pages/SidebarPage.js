e51ac68e4e5fd9be56660729a11337ee4c7768c0
sazidnxbs23 <Sazid.Ahmed@brainstation-23.com>
2026-04-30 14:38:40 +0600

    Update file

--- pages/SidebarPage.js
+++
@@ -1,9 +0,0 @@
-const SidebarPage = {
-  getSidebar: (page) => page.locator(".oxd-sidepanel"),
-  getMenuItemByName: (page, name) =>
-    page.locator(".oxd-main-menu-item", { hasText: name }),
-  getSearchInput: (page) =>
-    page.locator(".oxd-main-menu-search input"),
-};
-
-module.exports = SidebarPage;
